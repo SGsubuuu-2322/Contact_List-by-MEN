1debb8d531da8964f33a188814164aedfe6b9070
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-06 21:42:59 +0530

    Installed ejs as template engine and and created views folder and a view file to be rendered and then rendered it....

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const path = require("path");
 const port = 8000;
 
 const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Yup!!! buddy it is running</h1>");
+  
+
+    return res.render('home');
 });
 
 app.listen(port, (err) => {
