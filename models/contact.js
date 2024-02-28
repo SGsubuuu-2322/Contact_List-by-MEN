5310c24e7571295b1f4119e5380b5d0b37358b7e
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-28 22:54:05 +0530

    Added some relevant comments for contact.js model file...

--- models/contact.js
+++ models/contact.js
@@ -1,5 +1,7 @@
+// This is importing mongoose js as our ODM for model creation...
 const mongoose = require("mongoose");
 
+// This is creating schema for contacts collection , which will be used to create the Contact Model
 const contactSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -11,5 +13,6 @@ const contactSchema = new mongoose.Schema({
   },
 });
 
+// This is creating the model with  the above defined schema and exporting it so that
 const Contact = mongoose.model("Contact", contactSchema);
 module.exports = Contact;
