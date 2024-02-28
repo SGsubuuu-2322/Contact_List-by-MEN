ceea5fd0c7b1a0269902da37bf56bac5f48c4cc5
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-28 22:48:26 +0530

    Added relevant comments for index.js entry point file of our app...

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+// Importing express js in our project to be used as backend framework...
 const express = require("express");
+// Importing path for handling the file paths and setting port for our server to listen the clients request...
 const path = require("path");
 const port = 8000;
 
+// Importing mongoDB configurations  and connection with the database and contacts model.
 const db = require("./config/mongoose");
 const contact = require("./models/contact");
 
+// Creating an instance of express as app
 const app = express();
+
+// This is setting app views as ejs as their view-engine...
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// This  middleware function will help us handle data parsing from request body..
 app.use(express.urlencoded());
+// This is  a middleware function that will help us serve static as html, css and js...
 app.use(express.static("assets"));
 
 // let contactList = [
@@ -34,6 +43,7 @@ app.use(express.static("assets"));
 //   },
 // ];
 
+// This is controller for handling the home route with  HTTP GET method and some logic action and giving views as response...
 app.get("/", (req, res) => {
   contact
     .find({})
@@ -49,6 +59,8 @@ app.get("/", (req, res) => {
     });
 });
 
+
+// This is post request controller for creating contact and putting the contact details into the DB...
 app.post("/create-contact", (req, res) => {
   // contactList.push({
   //   name: req.body.name,
@@ -71,6 +83,9 @@ app.post("/create-contact", (req, res) => {
   // return res.redirect("back");
 });
 
+
+
+// This is get method controller for deleting a contact passed with specific contact id as query params
 app.get("/delete-contact/", (req, res) => {
   const id = req.query.id;
 
@@ -81,11 +96,15 @@ app.get("/delete-contact/", (req, res) => {
   return res.redirect("back");
 });
 
+// This is a get method controller for fun purpose...
 app.get("/practice", (req, res) => {
   return res.render("practice", {
     title: "Playing with ejs",
   });
 });
+
+
+// This is our server listening on specific port....
 app.listen(port, (err) => {
   if (err) {
     console.log("Error: ", err);
